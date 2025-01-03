a74b305655cc45dc6af4e9e80a6ac6ed97ceab3e
v.syrovyi <syrovyivitalii@gmail.com>
2025-01-03 12:11:03 +0200

    project created

---
+++ footballManager/src/main/java/lviv/syrovyi/footballManager/FootballManagerApplication.java
@@ -0,0 +1,13 @@
+package lviv.syrovyi.footballManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballManagerApplication.class, args);
+	}
+
+}
